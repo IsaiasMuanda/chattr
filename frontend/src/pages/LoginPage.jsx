6dbd61cea82a4c103b091f1f0d57d1cdb9b731a0
Isaias Marcos <143300660+IsaiasMuanda@users.noreply.github.com>
2025-04-28 01:38:15 +0100

    Alterações na página de login

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -60,9 +60,7 @@ const LoginPage = () => {
                 <label className="text-sm font-medium block">
                   Senha
                 </label>
-                <Link to="/forgot-password" className="text-xs text-primary hover:text-primary-focus transition-colors">
-                  Esqueceu a senha?
-                </Link>
+                
               </div>
               <div className="relative">
                 <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
